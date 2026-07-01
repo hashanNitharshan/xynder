bb4ff590afc343b77e505833298d1f3ec63d9e4b
Nitharshan Hashan <you@example.com>
2026-07-01 15:09:43 +0530

    make changes in pnl function and increase upload size

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,7 +14,7 @@ Route::options('{any}', function () {
 Route::get('/version', function () {
     return response()->json([
         'success'      => true,
-        'version'      => '1.0.2',
+        'version'      => '1.0.5',
         'apk_url'      => 'https://wallet.bitxnow.com/apk/wallet-mobile.apk',
         'force_update' => false,
     ]);
